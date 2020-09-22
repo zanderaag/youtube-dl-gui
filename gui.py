971c930791ee548c66c8cf44f57537ad5c5c37bd
Zander <zandersz922@gmail.com>
2020-09-22 15:51:51 -0400

    Added the meme picture as well as centered the Entry, Label, and Button widgets.

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+
 from ytdl import *
 from tkinter import *
 from tkinter.ttk import *
@@ -17,43 +18,36 @@ def printName():
 
 root = Tk()
 root.title("ytdl-gui")
-# root.geometry("250x250") #Set the window width and height
+root.geometry("700x633") #Set the window width and height
 root.resizable(0,0) #Set the window to be non-resizeable
 
 download_button_image = Image.open("images/download_button.png")
 download_button_resize = download_button_image.resize((15,15), Image.ANTIALIAS)
 download_button_tk = ImageTk.PhotoImage(download_button_resize)
 
+bg_image = PhotoImage(file="images/fuckspotify.png")
+img_label = Label(root, image=bg_image).place(relwidth=1, relheight=1)
 
-label1 = Label(root, text="Audio")
-entry1 = Entry(root)
-button1 = Button(root, image=download_button_tk)
+label1 = Label(root, text="Audio").grid(row=1, sticky="E")
+entry1 = Entry(root).grid(row=1, column=1)
+button1 = Button(root, image=download_button_tk).grid(row=1, column=2)
 
-label2 = Label(root, text="Video")
-entry2 = Entry(root)
-button2 = Button(root, image=download_button_tk)
+label2 = Label(root, text="Video").grid(row=2, sticky="E")
+entry2 = Entry(root).grid(row=2, column=1)
+button2 = Button(root, image=download_button_tk).grid(row=2, column=2)
 
-label3 = Label(root, text="Playlist Audio")
-entry3 = Entry(root)
-button3 = Button(root, image=download_button_tk)
+label3 = Label(root, text="Playlist Audio").grid(row=3, sticky="E")
+entry3 = Entry(root).grid(row=3, column=1)
+button3 = Button(root, image=download_button_tk).grid(row=3, column=2)
 
-label4 = Label(root, text="Playlist Video")
-entry4 = Entry(root)
-button4 = Button(root, image=download_button_tk)
+label4 = Label(root, text="Playlist Video").grid(row=4, sticky="E")
+entry4 = Entry(root).grid(row=4, column=1)
+button4 = Button(root, image=download_button_tk).grid(row=4, column=2)
 
-label1.grid(row=0, sticky=E)
-label2.grid(row=1, sticky=E)
-label3.grid(row=2, sticky=E)
-label4.grid(row=3, sticky=E)
-
-entry1.grid(row=0, column=1)
-entry2.grid(row=1, column=1)
-entry3.grid(row=2, column=1)
-entry4.grid(row=3, column=1)
-
-button1.grid(row=0, column=2)
-button2.grid(row=1, column=2)
-button3.grid(row=2, column=2)
-button4.grid(row=3, column=2)
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(5, weight=1)
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(5, weight=1)
 
 root.mainloop()
+

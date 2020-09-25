2256a14369eb07ae78ac56bbdd478e8c0f2ceb19
Zander <zandersz922@gmail.com>
2020-09-25 02:29:38 -0400

    Cleaned up further code, functionized the window creation code

--- gui.py
+++ gui.py
@@ -1,24 +1,10 @@
-# from ytdl import *
+import subprocess, os, time
 from tkinter import *
 from tkinter import filedialog
 from tkinter.ttk import *
 from PIL import ImageTk, Image
-import subprocess, os, time
 from time import sleep
 from shutil import copy
-
-result = "File done downloading."
-updatesuccess = "Requirements successfully updated."
-pathresultsuccess = "All three ffmpeg packages are in the following PATH."
-pathresultfail = "One or more ffmpeg packages are missing in your PATH.\n\
-Please ensure you have the following packages and you place them in one of the following folders:\nffmpeg.exe\nffplay.exe\nffprobe.exe\n"
-
-user = os.getlogin()
-unix_path = "/home/" + user + "/ytdl"
-win_path = "C:/Users/" + user + "/ytdl"
-folders = ['/audio-hq', '/video-hq', '/playlists-audio', '/playlists-video', '/streams', '/mp3', '/wav']
-bg_path = "images/"
-win_picture_path ="C:/Users/" + user + "/Pictures"
 
 def createFolderStructure():
 	if sys.platform == "win32":
@@ -72,7 +58,6 @@ def checkPATH():
 	else:
 		pass
 
-
 def changeBackgroundImage():
 	global bg_image, img_label
 
@@ -81,9 +66,12 @@ def changeBackgroundImage():
 
 	try:
 		with open("images/background.txt", "w") as f:
-			f.write(actual_file_name[1])
-			copy(root.filename, bg_path)
-	
+			if root.filename:
+				f.write(actual_file_name[1])
+				copy(root.filename, bg_path)
+			else:
+				f.write("bg1.jpg")
+
 		photo = Image.open(f"{root.filename}")
 		new_photo = photo.resize((640,480), Image.ANTIALIAS)
 		bg_image = ImageTk.PhotoImage(new_photo)
@@ -91,10 +79,15 @@ def changeBackgroundImage():
 	except:
 		pass
 
-	menu = Menu(root) #create main menu, in which seperate submenus exist
-	menu.option_add('*tearOff', False) #remove ----- in menu
+	createWindow(root)
+
+def createWindow(root):
+
+	menu = Menu(root) #Create main menu, in which seperate submenus exist
+	menu.option_add('*tearOff', False) #Remove ----- in menu
 
 	filemenu = Menu(menu)
+	menu.add_cascade(label="File", menu=filemenu)
 	filemenu.option_add('*tearOff', False)
 	filemenu.add_command(label="Open Audio Folder", command=openAudioFolder)
 	filemenu.add_command(label="Open Video Folder", command=openVideoFolder)
@@ -102,14 +95,13 @@ def changeBackgroundImage():
 	filemenu.add_command(label="Open Playlist Video Folder", command=openPlaylistVideoFolder)
 	filemenu.add_separator()
 	filemenu.add_command(label="Exit", command=quitprogram)
-	menu.add_cascade(label="File", menu=filemenu)
 
-	helpmenu = Menu(menu) #create help dropdown menu
+	helpmenu = Menu(menu) #Create help dropdown menu
+	menu.add_cascade(label="Help", menu=helpmenu) #Add submenu helpmenu to the main menu bar
 	helpmenu.option_add('*tearOff', False)
-	helpmenu.add_command(label="Check for updates", command=checkUpdates) #add check for updates box to the help menu
+	helpmenu.add_command(label="Check for updates", command=checkUpdates) #Add check for updates box to the help menu
 	helpmenu.add_command(label="Check PATH for dependencies", command=checkPATH)
 	helpmenu.add_command(label="Change background image", command=changeBackgroundImage)
-	menu.add_cascade(label="Help", menu=helpmenu) #add submenu helpmenu to the main menu bar
 
 	label1 = Label(root, text="Audio").grid(row=1, sticky="E")
 	entry1 = Entry(root, width=70, textvariable=audiovar).grid(row=1, column=1)
@@ -127,12 +119,12 @@ def changeBackgroundImage():
 	entry4 = Entry(root, width=70, textvariable=playlistvideovar).grid(row=4, column=1)
 	button4 = Button(root, image=download_button_tk, command=lambda: downloadPlaylistVideo()).grid(row=4, column=2)
 
-	root.grid_rowconfigure(0, weight=1)
+	root.grid_rowconfigure(0, weight=1)#This configures the grid so the 1st and last row are not used, allowing centering in the grid
 	root.grid_rowconfigure(5, weight=1)
-	root.grid_columnconfigure(0, weight=1)
+	root.grid_columnconfigure(0, weight=1)#This configures the grid so the 1st and last column are not used, allowing centering in the grid
 	root.grid_columnconfigure(5, weight=1)
 
-	root.title("YouTube-DL-GUI")
+	root.title("YouTube-DL-GUI")#Set the title of the window
 	root.geometry("640x480") #Set the window width and height
 	root.resizable(0,0) #Set the window to be non-resizeable
 	root.config(menu=menu)
@@ -210,8 +202,21 @@ def downloadPlaylistVideo():
 def quitprogram():
 	root.destroy()
 
-createFolderStructure()
+#string variables
+user = os.getlogin()
+unix_path = "/home/" + user + "/ytdl"
+win_path = "C:/Users/" + user + "/ytdl"
+folders = ['/audio-hq', '/video-hq', '/playlists-audio', '/playlists-video']
+bg_path = "images/"
+win_picture_path ="C:/Users/" + user + "/Pictures"
+result = "File done downloading."
+updatesuccess = "Requirements successfully updated."
+pathresultsuccess = "All three ffmpeg packages are in the following PATH."
+pathresultfail = "One or more ffmpeg packages are missing in your PATH.\n\
+Please ensure you have the following packages and you place them in one of the following folders:\nffmpeg.exe\nffplay.exe\nffprobe.exe\n"
 
+#create folder structure and instantiate Tk object
+createFolderStructure()
 root = Tk()
 
 #variables for the Entry objects
@@ -236,56 +241,12 @@ try:
 	bg_image_resized = ImageTk.PhotoImage(bg_image_2)
 	img_label = Label(root, image=bg_image_resized, relief=SOLID).place(relwidth=1, relheight=1)
 except:
-	bg_image = Image.open(f"images/bg1.jpg")
-	bg_image_2 = bg_image.resize((640, 480), Image.ANTIALIAS)
-	bg_image_resized = ImageTk.PhotoImage(bg_image_2)
-	img_label = Label(root, image=bg_image_resized, relief=SOLID).place(relwidth=1, relheight=1)
+	try:
+		bg_image = Image.open(f"images/bg1.jpg")
+		bg_image_2 = bg_image.resize((640, 480), Image.ANTIALIAS)
+		bg_image_resized = ImageTk.PhotoImage(bg_image_2)
+		img_label = Label(root, image=bg_image_resized, relief=SOLID).place(relwidth=1, relheight=1)
+	except:
+		print("Please place an image named bg1.jpg into the images folder if you encounter further issues.")
 
-menu = Menu(root) #create main menu, in which seperate submenus exist
-menu.option_add('*tearOff', False) #remove ----- in menu
-
-filemenu = Menu(menu)
-filemenu.option_add('*tearOff', False)
-filemenu.add_command(label="Open Audio Folder", command=openAudioFolder)
-filemenu.add_command(label="Open Video Folder", command=openVideoFolder)
-filemenu.add_command(label="Open Playlist Audio Folder", command=openPlaylistAudioFolder)
-filemenu.add_command(label="Open Playlist Video Folder", command=openPlaylistVideoFolder)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=quitprogram)
-menu.add_cascade(label="File", menu=filemenu)
-
-helpmenu = Menu(menu) #create help dropdown menu
-helpmenu.option_add('*tearOff', False)
-helpmenu.add_command(label="Check for updates", command=checkUpdates) #add check for updates box to the help menu
-helpmenu.add_command(label="Check PATH for dependencies", command=checkPATH)
-helpmenu.add_command(label="Change background image", command=changeBackgroundImage)
-menu.add_cascade(label="Help", menu=helpmenu) #add submenu helpmenu to the main menu bar
-
-label1 = Label(root, text="Audio").grid(row=1, sticky="E")
-entry1 = Entry(root, width=70, textvariable=audiovar).grid(row=1, column=1)
-button1 = Button(root, image=download_button_tk, command=lambda: downloadAudioHighestQuality()).grid(row=1, column=2)
-
-label2 = Label(root, text="Video").grid(row=2, sticky="E")
-entry2 = Entry(root, width=70, textvariable=videovar).grid(row=2, column=1)
-button2 = Button(root, image=download_button_tk, command=lambda: downloadVideoHighestQuality()).grid(row=2, column=2)
-
-label3 = Label(root, text="Playlist Audio").grid(row=3, sticky="E")
-entry3 = Entry(root, width=70, textvariable=playlistaudiovar).grid(row=3, column=1)
-button3 = Button(root, image=download_button_tk, command=lambda: downloadPlaylistAudio()).grid(row=3, column=2)
-
-label4 = Label(root, text="Playlist Video").grid(row=4, sticky="E")
-entry4 = Entry(root, width=70, textvariable=playlistvideovar).grid(row=4, column=1)
-button4 = Button(root, image=download_button_tk, command=lambda: downloadPlaylistVideo()).grid(row=4, column=2)
-
-root.grid_rowconfigure(0, weight=1)
-root.grid_rowconfigure(5, weight=1)
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(5, weight=1)
-
-root.title("YouTube-DL-GUI")
-root.geometry("640x480") #Set the window width and height
-root.resizable(0,0) #Set the window to be non-resizeable
-root.config(menu=menu)
-
-root.mainloop()
-
+createWindow(root)
